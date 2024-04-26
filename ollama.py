07fee4b8a2cf497ab00857aaab075f7fe1fc5be1
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-04-26 20:55:46 +0530

    created Ollama model

---
+++ ollama.py
@@ -0,0 +1,31 @@
+import os
+import streamlit as st
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_community.llms import Ollama
+
+GOOGLE_API_KEY = os.environ.get("GOOGLE_API_KEY")
+LANGCHAIN_TRACING_V2="true"
+LANGCHAIN_API_KEY=os.environ.get("LANGCHAIN_API_KEY")
+
+
+#Ollama model
+llm = Ollama(model="llama2")
+
+
+prompt = ChatPromptTemplate.from_messages([
+        ("system", "You are a helpful assistant."),
+        ("user", "Question:{question}"),
+])
+output_parser = StrOutputParser()
+chain = prompt|llm|output_parser
+
+
+#Streamlit
+import streamlit as st
+st.title("Google Gemini 1.5 pro model chatbot")
+question = st.text_input("Ask me a Question")
+
+if question:
+    st.write(chain.invoke(f"Question: {question}")) 
